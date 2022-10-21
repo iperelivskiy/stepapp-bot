fdeb22908957e8496649d93af42633c790bb384f
Ivan Perelivskiy <iperelivskiy@gmail.com>
2022-10-22 01:37:04 +0600

    Minor

--- app/buyer.py
+++ app/buyer.py
@@ -114,7 +114,7 @@ async def main():
     bot = await client.start(bot_token=TELEGRAM_BOT_TOKEN)
 
     async def check_sellings_loop(bot):
-        current_sellings = 10
+        current_sellings = None
 
         while True:
             try:

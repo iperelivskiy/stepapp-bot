30dcca6db5861221bf7f6e6f27fee3670449d617
Ivan Perelivskiy <iperelivskiy@gmail.com>
2022-12-23 17:44:01 +0600

    LB prices updated

--- app/watcher.py
+++ app/watcher.py
@@ -117,8 +117,12 @@ async def check_lootboxes(redis, session, bot, set_aggressive_mode):
             # Gen 1-3
             return True
 
-        if item['networkTokenId'] < 400000 and item['priceFitfi'] <= 300:
+        if item['networkTokenId'] < 400000 and item['priceFitfi'] <= 350:
             # Edition3 and earlier
+            return True
+
+        if item['networkTokenId'] < 490000 and item['priceFitfi'] <= 250:
+            # Edition4
             return True
 
         return False
